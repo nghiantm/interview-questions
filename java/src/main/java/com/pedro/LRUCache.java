3d5541180f5988cd47fac7d121862872202f2690
Matt Nguyen <nguyentrongminhnghia@gmail.com>
2025-01-14 21:59:12 -0500

    Wrote LRU implementation in Java. Modified Dockerfile to make sure everything is copied, and put java and test file in com/pedro to match groupId

--- java/src/main/java/com/pedro/LRUCache.java
+++ java/src/main/java/com/pedro/LRUCache.java
@@ -1,9 +1,12 @@
+package com.pedro;
+
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class LRUCache {
 
-    // store keys of cache
+    // store keys of cache (new -> old)
     static Deque<Integer> dq;
 
     // store references of key in cache
@@ -22,10 +25,22 @@ public class LRUCache {
     void refer(int x)
     {
         // not present in cache
-
+        if (!map.contains(x)) {
+            // cache is full
+            if (dq.size() == csize) {
+                // delete least recently used element
+                dq.removeLast();
+                map.remove(x);
+            }
+        }
         // present in cache
-
+        else {
+            // move x to recently used
+            dq.remove(x);
+        }
         // update reference
+        dq.addFirst(x);
+        map.add(x);
     }
 
     // display contents of cache
--- java/src/test/java/com/pedro/LRUCacheTest.java
+++ java/src/test/java/com/pedro/LRUCacheTest.java
@@ -1,8 +1,9 @@
+package com.pedro;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 public class LRUCacheTest {
 
